917b9241ab760f7d0f5f7afe550ece688322dbe1
konarbalaji <konar.balaji@gmail.com>
2023-04-13 22:01:38 +0530

    Added new class for bounded type parameters

---
+++ Collections_Generic_Reflections_in_Java/src/main/java/_02_BoundedTypeParameters/BoundedClass1.java
@@ -0,0 +1,65 @@
+package _02_BoundedTypeParameters;
+
+class Bound<T extends A>{
+
+    private T objRef;
+
+    public Bound(T objRef) {
+        this.objRef = objRef;
+    }
+
+    public void doRunTest(){
+        this.objRef.displayClass();
+    }
+}
+
+class A{
+
+    public void displayClass(){
+        System.out.println("Inside class A");
+    }
+
+}
+
+class B extends A{
+
+    public void displayClass(){
+        System.out.println("Inside class B");
+    }
+
+}
+
+class C extends A{
+
+    public void displayClass(){
+        System.out.println("Inside class C");
+    }
+
+}
+
+class D {
+
+    public void displayClass(){
+        System.out.println("Inside class C");
+    }
+
+}
+
+
+
+
+public class BoundedClass1 {
+
+    public static void main(String[] args) {
+
+        Bound<C> bc = new Bound<>(new C());
+        bc.doRunTest();
+
+        Bound<B> bb = new Bound<>(new B());
+        bb.doRunTest();
+
+        Bound<A> ba = new Bound<>(new A());
+        ba.doRunTest();
+
+    }
+}
