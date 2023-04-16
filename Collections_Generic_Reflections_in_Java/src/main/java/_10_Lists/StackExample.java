468f11fee08ff78babdcd9e55e6e547dfd3a3e2b
konarbalaji <konar.balaji@gmail.com>
2023-04-16 23:05:34 +0530

    added class for Stack

---
+++ Collections_Generic_Reflections_in_Java/src/main/java/_10_Lists/StackExample.java
@@ -0,0 +1,19 @@
+package _10_Lists;
+
+import java.util.Stack;
+
+public class StackExample {
+    public static void main(String[] args) {
+        Stack<String> names = new Stack<>();
+
+        names.push("Adam");
+        names.push("Joe");
+        names.push("Ana");
+        names.push("Daniel");
+        names.push("Katy");
+
+        while (!names.isEmpty())
+            System.out.println(names.pop());
+
+    }
+}
